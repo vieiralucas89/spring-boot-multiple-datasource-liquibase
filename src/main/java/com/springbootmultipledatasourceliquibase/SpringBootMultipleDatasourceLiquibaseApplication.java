8f510deaa5470587b373fcfda60e7ee6f4c33eaa
lucas.vieira <vyeiralucas@gmail.com>
2020-03-25 20:04:02 -0300

    first commit

---
+++ src/main/java/com/springbootmultipledatasourceliquibase/SpringBootMultipleDatasourceLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmultipledatasourceliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultipleDatasourceLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultipleDatasourceLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootmultipledatasourceliquibase/SpringBootMultipleDatasourceLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmultipledatasourceliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultipleDatasourceLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
